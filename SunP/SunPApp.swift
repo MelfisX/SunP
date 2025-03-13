54a045656b4dbc06494588495b8292f409997d8d
MelfisX <melfis@yandex.ru>
2025-03-13 15:54:35 +0300

    Initial Commit

---
+++ SunP/SunPApp.swift
@@ -0,0 +1,17 @@
+//
+//  SunPApp.swift
+//  SunP
+//
+//  Created by Mikhail Veselov on 13.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct SunPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SunPTests/SunPTests.swift
@@ -0,0 +1,16 @@
+//
+//  SunPTests.swift
+//  SunPTests
+//
+//  Created by Mikhail Veselov on 13.03.2025.
+//
+
+import Testing
+
+struct SunPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
